995944940a67495d008ae6921a1ac69fc4682da0
Alphasf9 <134485025+Alphasf9@users.noreply.github.com>
2025-01-14 11:39:19 +0530

    remove password field from the profile response

--- user/middlewares/authMiddleware.js
+++ user/middlewares/authMiddleware.js
@@ -19,7 +19,7 @@ export const userAuth = async (req, res, next) => {
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        const user = await userModel.findById(decoded.id);
+        const user = await userModel.findById(decoded.id).select('-password');
 
         if (!user) {
             return res.status(401).json({ message: 'Unauthorized' });
